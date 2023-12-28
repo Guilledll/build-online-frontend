7e2505d6ea35d24e3162f01483fbfcd0509a8562
Guilledll <guilledll20@gmail.com>
2023-12-28 08:56:51 -0300

    organize contact pages and components

---
+++ components/contact/DataForm.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    Component: contact/DataForm
+  </div>
+</template>
+
+<style scoped></style>
---
+++ components/contact/ProfileBanner.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    Component: contact/ProfileBanner
+  </div>
+</template>
+
+<style scoped></style>
