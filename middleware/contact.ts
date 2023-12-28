4811205f4f18f6e3b2dd0f771cc93b7cefe98012
Guilledll <guilledll20@gmail.com>
2023-12-28 13:14:28 -0300

    improve middleware

--- middleware/contact.ts
+++ middleware/contact.ts
@@ -1,7 +1,10 @@
 export default defineNuxtRouteMiddleware(async (to) => {
     const store = useContactStore();
 
-    await store.getContact(to.params.id);
+    // Fetch contact only if it is not loaded yet
+    if (!store.contact || store.contact?.id !== Number(to.params.id)) {
+        await store.getContact(to.params.id);
+    }
 
     if (!store.contact) return navigateTo('/contacts');
 })
